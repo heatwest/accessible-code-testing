a747de8fe563ed5fe32d726fd165e906a507e1cb
Heather Westerlund <65722388+heatwest@users.noreply.github.com>
2022-11-08 16:04:30 -0600

    button testing

---
+++ misc.js
@@ -0,0 +1,12 @@
+/* the jQuery click() event can be activated by mouse or keyboard */
+$("[role=link]").click(function(){
+    var href = $(this).attr('data-href');
+    window.open(href);
+ });
+ /* Note: if you don't want the link to open in a new window,
+ use document.location = href;
+ instead of window.open(href) */
+
+  $('#button-9111').click(function(e) {
+  alert("You activated the native button with a click action!");
+});
